d1a3d6f74209c8ebda4022687576deb64cdd26fe
Alexey Shumkin <Alex.Crezoff@gmail.com>
2019-03-19 01:50:37 +0300

    improve: process commands via reflection. Add /start, /help commands

--- src/main/java/ru/zapped/tgbot/quickbuild/TGBotQuickbuild.java
+++ src/main/java/ru/zapped/tgbot/quickbuild/TGBotQuickbuild.java
@@ -6,6 +6,21 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import javax.swing.plaf.synth.SynthTextAreaUI;
+import javax.xml.bind.Element;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Method;
+
+@Target(ElementType.METHOD)
+@Retention(value= RetentionPolicy.RUNTIME)
+@interface BotCommand {
+    String Command();
+    String Description();
+}
+
 public class TGBotQuickbuild extends TelegramLongPollingBot
 {
     private String fbotName = null;
@@ -20,18 +35,94 @@ public class TGBotQuickbuild extends TelegramLongPollingBot
         }
     }
 
+    @BotCommand(Command="/start", Description = "Starts interacting with the bot")
+    public void onCommandStart(Update update) {
+        SendMessage message = createReply(update)
+            .setText(String.format("Bot started to talk to %s", update.getMessage().getFrom().toString()));
+        sendMessage(message);
+    }
+
+    @BotCommand(Command = "/help", Description = "Shows this help")
+    public void helpCommand(Update update) {
+        SendMessage message = createReply(update)
+                .setText(String.format("Usage:\n%s", getHelp()));
+        sendMessage(message);
+    }
+
+    public void unknownCommand(Update update) {
+        helpCommand(update);
+    }
+
+    private String getHelp() {
+        StringBuilder result = new StringBuilder();
+        for(Method method: this.getClass().getMethods()) {
+            if (method.isAnnotationPresent(BotCommand.class)) {
+                BotCommand command = method.getAnnotation(BotCommand.class);
+                result.append(command.Command());
+                result.append(" ");
+                result.append(command.Description());
+                result.append("\n");
+            }
+        }
+        return result.toString();
+    }
+
+    private void sendMessage(SendMessage message) {
+        try {
+            execute(message); // Call method to send the message
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private SendMessage createReply(Update update) {
+        return new SendMessage() // Create a SendMessage object with mandatory fields
+                .setChatId(update.getMessage().getChatId());
+    }
+
+    private Method findCommand(String commandText) {
+        for(Method method: this.getClass().getMethods()) {
+            if (method.isAnnotationPresent(BotCommand.class)) {
+                BotCommand command = method.getAnnotation(BotCommand.class);
+                if (command.Command().equalsIgnoreCase(commandText)) {
+                    return method;
+                }
+            }
+        }
+        try {
+            return this.getClass().getMethod("unknownCommand", Update.class);
+        } catch (NoSuchMethodException e) {
+            return null;
+        }
+    }
+
+    private void executeCommand(Method commandMethod, Update update) {
+        try {
+            commandMethod.invoke(this, update);
+        } catch (Exception e) {
+
+        }
+    }
+
+    private String extractCommand(String messageText) {
+        String[] tokens = StringUtils.split(messageText, " ", 2);
+        return tokens[0];
+    }
+
+    private void testForCommand(Update update) {
+        String extractedCommand = null;
+        if (update.getMessage().isCommand()) {
+            extractedCommand = extractCommand(update.getMessage().getText());
+        }
+        Method commandMethod = findCommand(extractedCommand);
+        executeCommand(commandMethod, update);
+    }
+
     @Override
     public void onUpdateReceived(Update update) {
         // We check if the update has a message and the message has text
         if (update.hasMessage() && update.getMessage().hasText()) {
-            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
-                    .setChatId(update.getMessage().getChatId())
-                    .setText(update.getMessage().getText());
-            try {
-                execute(message); // Call method to send the message
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
-            }
+            testForCommand(update);
         }
     }
 
